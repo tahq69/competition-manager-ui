0939fe5c805f91766a16cf11a02e4453420f2682
Igors Krasjukovs <tahq69@gmail.com>
2018-02-18 16:06:47 +0200

    Rename index files to lower case;

---
+++ src/Config/index.ts
@@ -0,0 +1,17 @@
+import ILoggerOptions from "./LoggerOptions"
+import settings from "./settings.json"
+
+interface IConfig {
+  auth_secret: string
+  auth_token_type: string
+  auth_id: number
+  url: string
+  api_url: string
+  server_date_format: string
+  default_locale: string
+  logs: ILoggerOptions
+}
+
+const config = settings[settings["env"]] as IConfig
+
+export default config
