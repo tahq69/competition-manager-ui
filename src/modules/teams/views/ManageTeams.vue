7885ec8fd62192c3b6e5c39f9c1327faca2de549
Igors Krasjukovs <tahq69@gmail.com>
2018-04-01 17:18:29 +0300

    Update teams list grid view with async paging;

--- src/modules/teams/views/ManageTeams.vue
+++ src/modules/teams/views/ManageTeams.vue
@@ -1,67 +1,67 @@
 <script lang="ts">
-import { Paging } from "crip-vue-bootstrap"
+import { createPaging } from "crip-vue-bootstrap"
 import Vue from "vue"
 import { Location } from "vue-router"
 
+import { Id } from "@/types"
 import { createTeam, manageTeams } from "@/router/routes"
 
+import { manageTeamMembersRoute } from "../members/routes"
+import { createTeamRoute, manageTeamRoute } from "../routes"
 import teamService from "../service"
 import { Team } from "../team"
+
+const { mixin, paging: teams } = createPaging<Team>(paging => teamService.fetchTeams({ paging }))
 
 export default Vue.extend({
   name: "ManageTeams",
 
-  data() {
-    const vm = this
-    return {
-      createRoute: createTeam,
-      paging: new Paging<Team>({ vm, route: manageTeams }),
-    }
+  mixins: [mixin],
+
+  data: () => ({ teams }),
+
+  computed: {
+    createTeamRoute(): any {
+      return createTeamRoute()
+    },
+  },
+
+  methods: {
+    manageTeamRoute: (id: Id) => manageTeamRoute({ team: id }),
+    manageTeamMembersRoute: (id: Id) => manageTeamMembersRoute({ team: id }),
   },
 
   created() {
     this.$logger.component(this)
-    this.paging.init(() => this.fetchPage())
-  },
-
-  methods: {
-    async fetchPage() {
-      const pagination = await teamService.fetchTeams({
-        paging: this.paging,
-        managed: true,
-      })
-
-      this.paging.update(pagination)
-    },
   },
 })
 </script>
 
 <template>
   <CGrid id="manage-teams"
-         :paging="paging">
+         :paging="teams">
     <span slot="title">{{ $t('teams.manage_teams_grid_title') }}</span>
     <CCardAction slot="actions"
-                 :to="createRoute">
+                 :to="createTeamRoute">
       {{ $t('teams.manage_teams_grid_head_create_new') }}
     </CCardAction>
 
     <table class="table table-hover">
       <thead>
         <tr>
-          <CGridHeader :paging="paging"
+          <CGridHeader :paging="teams"
                        column="id"
                        :title="$t('teams.manage_teams_grid_head_id_title')">
             {{ $t('teams.manage_teams_grid_head_id_text') }}
           </CGridHeader>
 
-          <CGridHeader :paging="paging"
+          <CGridHeader :paging="teams"
                        column="name"
                        :title="$t('teams.manage_teams_grid_head_name_title')">
             {{ $t('teams.manage_teams_grid_head_name_text') }}
           </CGridHeader>
 
-          <CGridHeader :paging="paging"
+          <CGridHeader :paging="teams"
                        column="short"
                        :title="$t('teams.manage_teams_grid_head_short_title')">
             {{ $t('teams.manage_teams_grid_head_short_text') }}
@@ -69,20 +69,20 @@ export default Vue.extend({
         </tr>
       </thead>
       <tbody>
-        <template v-for="team in paging.items">
-          <tr @click="paging.select(team)"
-              :class="paging.classes(team)"
+        <template v-for="team in teams.items">
+          <tr @click="teams.select(team)"
+              :class="teams.classes(team)"
               :key="team.id">
             <td>{{ team.id }}</td>
             <td>
               {{ team.name }} &nbsp;
-              <router-link :to="team.routes.edit"
+              <router-link :to="manageTeamRoute(team.id)"
                            class="badge badge-light actions"
                            :title="$t('teams.manage_teams_grid_btn_edit_title')">
                 <i class="fa fa-pencil-square-o"></i>
                 {{ $t('teams.manage_teams_grid_btn_edit_text') }}
               </router-link>
-              <router-link :to="team.routes.manageMembers"
+              <router-link :to="manageTeamMembersRoute(team.id)"
                            class="badge badge-light actions"
                            :title="$t('teams.manage_teams_grid_btn_members_title')">
                 {{ $t('teams.manage_teams_grid_btn_members_text') }}
