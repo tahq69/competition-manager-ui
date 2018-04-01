e6d0f8d6f710c455f117c876192d97a48b4080c8
Igors Krasjukovs <tahq69@gmail.com>
2018-04-01 19:42:44 +0300

    Edit btn in team details view;

--- src/modules/teams/views/Team.vue
+++ src/modules/teams/views/Team.vue
@@ -3,19 +3,19 @@ import Vue from "vue"
 
 import { Next } from "@/types"
 
+import { TeamAuth } from "../auth"
 import { Team } from "../models/team"
 import teamService from "../service"
 
 import TeamCompetitionsLink from "../links/TeamCompetitionsLink.vue"
 import TeamLink from "../links/TeamLink.vue"
 
+import { manageTeamRoute } from "../routes"
+
 export default Vue.extend({
   name: "Team",
 
-  components: {
-    TeamCompetitionsLink,
-    TeamLink,
-  },
+  components: { TeamCompetitionsLink, TeamLink },
 
   props: {
     team: { type: [Number, String], required: true },
@@ -26,12 +26,15 @@ export default Vue.extend({
     teamService.fetchTeam(payload).then(team => next(vm => vm.setTeam(team)))
   },
 
-  data: () => ({ details: new Team({}) }),
+  data: () => ({ details: new Team({}), canEdit: false }),
 
   methods: {
-    setTeam(team: Team) {
+    async setTeam(team: Team) {
       this.details = team
+      this.canEdit = await TeamAuth.canEdit({ team: team.id })
     },
+
+    manageTeamRoute: (team: Team) => manageTeamRoute({ team: team.id }),
   },
 
   mounted() {
@@ -51,7 +54,15 @@ export default Vue.extend({
              alt="Team logo">
         <div class="card-body">
           <h5 class="card-title">{{ details.name }}</h5>
-          <p class="card-text">{{ details.short }}</p>
+          <p class="card-subtitle mb-2 text-muted">{{ details.short }}</p>
+        </div>
+        <div v-if="canEdit"
+             class="card-footer">
+          <router-link :to="manageTeamRoute(details)"
+                       class="btn btn-primary">
+            <i class="fa fa-pencil-square-o"></i>
+            <span>Edit</span>
+          </router-link>
         </div>
       </div>
     </CCol>
