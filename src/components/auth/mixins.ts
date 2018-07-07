a8dd929ad046e3346540d5148413cb2e6d656b1d
Igors Krasjukovs <tahq69@gmail.com>
2018-07-07 14:36:16 +0300

    Simplify auth mixin;

--- src/components/auth/mixins.ts
+++ src/components/auth/mixins.ts
@@ -1,41 +1,30 @@
-import { IState } from "@/store/typings";
-
 export const watchVisibility = {
   data: () => ({ isVisible: false }),
 
+  methods: {
+    __checkVisibility() {
+      const component: any = this;
+      if (
+        component.checkVisibility &&
+        typeof component.checkVisibility === "function"
+      ) {
+        component.checkVisibility().then((isVisible: boolean) => {
+          component.isVisible = isVisible;
+        });
+      }
+    }
+  },
+
   created() {
     const component: any = this;
-    if (
-      component.checkVisibility &&
-      typeof component.checkVisibility === "function"
-    ) {
-      component.$store.watch(
-        (state: IState) => state.auth.user.authenticated,
-        (newValue: boolean, oldValue: boolean) => {
-          if (!newValue) {
-            // if user log out, make all components invisible.
-            component.isVisible = false;
-          } else {
-            // otherwyse just recheck visibility as this may be initial public
-            // view load and auth status arrives later than component is mounted
-            component.checkVisibility().then((isVisible: boolean) => {
-              component.isVisible = isVisible;
-            });
-          }
-        }
-      );
-    }
+    component.$watch(
+      () => component.$store.state.auth.user.authenticated,
+      () => component.__checkVisibility()
+    );
   },
 
   mounted() {
     const component: any = this;
-    if (
-      component.checkVisibility &&
-      typeof component.checkVisibility === "function"
-    ) {
-      component.checkVisibility().then((isVisible: boolean) => {
-        component.isVisible = isVisible;
-      });
-    }
+    component.__checkVisibility();
   }
 };
