009db74e1ffaa46cbde86130e3099982a0f946f2
Igors Krasjukovs <tahq69@gmail.com>
2018-07-16 21:52:14 +0300

    Fix manage team member list issues;

--- src/modules/teams/members/views/ManageMembers.vue
+++ src/modules/teams/members/views/ManageMembers.vue
@@ -1,167 +1,117 @@
 <script lang="ts">
 import Vue from "vue";
 
-import { Paging, SortDirection } from "@/helpers";
+import { Paging } from "@/helpers";
 import { manageTeamMembers } from "@/router/routes";
+import { table } from "@/components/mixins";
 
 import { TeamMember } from "#/teams/models/team-member";
 import { fetchTeamMembers } from "#/teams/members/service";
 
-import ManageTeamMemberBtn from "#/teams/components/ManageTeamMemberBtn.vue";
-import CreateTeamMemberBtn from "#/teams/components/CreateTeamMemberBtn.vue";
-import TeamBtn from "#/teams/components/TeamBtn.vue";
-import ManageTeamBtn from "#/teams/components/ManageTeamBtn.vue";
+import CreateTeamMemberLink from "#/teams/components/CreateTeamMemberLink.vue";
+import ManageTeamLink from "#/teams/components/ManageTeamLink.vue";
+import ManageTeamMemberLink from "#/teams/components/ManageTeamMemberLink.vue";
+import TeamLink from "#/teams/components/TeamLink.vue";
 
 export default Vue.extend({
   name: "ManageMembers",
 
   components: {
-    ManageTeamMemberBtn,
-    CreateTeamMemberBtn,
-    TeamBtn,
-    ManageTeamBtn
+    CreateTeamMemberLink,
+    ManageTeamLink,
+    ManageTeamMemberLink,
+    TeamLink
   },
+
+  mixins: [table],
 
   props: {
-    team: { type: [String, Number], required: true },
-    page: { type: [String, Number], required: true },
-    pageSize: { type: [String, Number], required: true },
-    sort: { type: String, required: true },
-    direction: { type: String, required: true }
+    team: { type: [String, Number], required: true }
   },
 
-  data: () => ({
-    loading: false,
-    totalItems: 1000000,
-    members: [] as TeamMember[]
-  }),
-
-  computed: {
-    currentPage(): number {
-      // convert route string parameter to number for el-pagination.
-      return parseInt(this.page.toString()) || 1;
-    },
-
-    currentPageSize(): number {
-      // convert route string parameter to number for el-pagination.
-      return parseInt(this.pageSize.toString()) || 10;
-    }
-  },
+  data: () => ({ members: [] as TeamMember[] }),
 
   methods: {
-    currentChange(page: string) {
-      const pageSize = this.pageSize.toString();
-
-      // trigger route change when users updates pagination.
-      this.$router.push({
-        name: manageTeamMembers.name,
-        params: { page, sort: this.sort, direction: this.direction, pageSize }
-      });
-    },
-
-    async fetchPage() {
-      this.loading = true;
-
-      const page = this.currentPage;
-      const pageSize = this.currentPageSize;
-      const direction = this.direction as SortDirection;
-      const paging = new Paging(page, pageSize, this.sort, direction);
+    async fetchPage(paging: Paging) {
       const payload = { paging, team_id: this.team };
       const paginated = await fetchTeamMembers(payload);
 
       this.members = paginated.items;
-      this.totalItems = paginated.total;
 
-      this.loading = false;
+      return paginated.total;
     }
-  },
-
-  created() {
-    this.log = this.$logger.component(this);
-
-    // fetch data on component initial load.
-    this.fetchPage();
-
-    // update when page/pageSize/sort/direction is changed.
-    this.$watch(
-      () => [this.page, this.sort, this.direction, this.pageSize].join(),
-      () => this.fetchPage()
-    );
   }
 });
 </script>
 
 <template>
-  <CGrid id="manage-members"
-         :paging="members">
-    <span slot="title">{{ $t('teams.manage_members_grid_title') }}</span>
-
-    <span slot="actions">
-      <ManageTeamBtn :team="team"
-                     btn="light"
-                     title="Edit team details"
-                     arrow="left"
-                     icon>
+  <el-card id="manage-team-members">
+    <div slot="header"
+         class="clearfix">
+      <span>{{ $t('teams.manage_members_grid_title') }}</span>
+      <ManageTeamLink title="Edit team details"
+                      icon="edit"
+                      :team="team"
+                      button
+                      mini>
         {{ $t('teams.manage_members_grid_head_manage_team') }}
-      </ManageTeamBtn>
-
-      <TeamBtn :team="team"
-               btn="light"
-               title="View team public profile"
-               arrow="left"
-               icon>
+      </ManageTeamLink>
+      <TeamLink title="View team public profile"
+                icon="view"
+                :team="team"
+                button
+                mini>
         Team
-      </TeamBtn>
-
-      <CreateTeamMemberBtn :team="team"
-                           btn="light"
-                           title="Create new member in a team"
-                           icon>
+      </TeamLink>
+      <CreateTeamMemberLink title="Create new member in a team"
+                            icon="plus"
+                            :team="team"
+                            button
+                            mini>
         {{ $t('teams.manage_members_grid_head_create_member') }}
-      </CreateTeamMemberBtn>
-    </span>
+      </CreateTeamMemberLink>
+    </div>
+    <div v-loading="loading">
+      <el-table :data="members"
+                :default-sort="defaultSort"
+                @sort-change="onSortChange">
+        <el-table-column prop="id"
+                         :label="$t('teams.manage_members_grid_head_id')"
+                         sortable="custom">
+        </el-table-column>
+        <el-table-column prop="name"
+                         :label="$t('teams.manage_members_grid_head_name')"
+                         sortable="custom">
+        </el-table-column>
+        <el-table-column prop="membership_type"
+                         :label="$t('teams.manage_members_grid_head_membership_type')"
+                         sortable="custom">
+        </el-table-column>
+        <el-table-column>
+          <template slot-scope="member">
+            <ManageTeamMemberLink :team="member.row.team_id"
+                                  :member="member.row.id"
+                                  :title="$t('teams.manage_members_grid_btn_edit_title')"
+                                  type="primary"
+                                  icon="edit"
+                                  button
+                                  circle
+                                  mini>
+            </ManageTeamMemberLink>
+          </template>
+        </el-table-column>
+      </el-table>
 
-    <table class="table table-hover">
-      <thead>
-        <tr>
-          <CGridHeader :paging="members"
-                       column="id"
-                       :title="$t('teams.manage_members_grid_head_id_title')">
-            {{ $t('teams.manage_members_grid_head_id_text') }}
-          </CGridHeader>
-
-          <CGridHeader :paging="members"
-                       column="name"
-                       :title="$t('teams.manage_members_grid_head_name_title')">
-            {{ $t('teams.manage_members_grid_head_name_text') }}
-          </CGridHeader>
-
-          <CGridHeader :paging="members"
-                       column="membership_type"
-                       :title="$t('teams.manage_members_grid_head_membership_type_title')">
-            {{ $t('teams.manage_members_grid_head_membership_type_text') }}
-          </CGridHeader>
-        </tr>
-      </thead>
-      <tbody>
-        <template v-for="member in members">
-          <tr @click="members.select(member)"
-              :class="members.classes(member)"
-              :key="member.id">
-            <td>{{ member.id }}</td>
-            <td>{{ member.name }} &nbsp;
-              <ManageTeamMemberBtn :team="member.team_id"
-                                   :member="member.id"
-                                   :title="$t('teams.manage_members_grid_btn_edit_title')"
-                                   badge
-                                   icon>
-                {{ $t('teams.manage_members_grid_btn_edit_text') }}
-              </ManageTeamMemberBtn>
-            </td>
-            <td>{{ member.membership_type }}</td>
-          </tr>
-        </template>
-      </tbody>
-    </table>
-  </CGrid>
+      <el-row class="pagination-row">
+        <el-pagination @current-change="onPageChange"
+                       @size-change="onPageSizeChange"
+                       layout="total, prev, pager, next, sizes"
+                       :current-page="currentPage"
+                       :page-sizes="pageSizes"
+                       :page-size="currentPageSize"
+                       :total="totalItems">
+        </el-pagination>
+      </el-row>
+    </div>
+  </el-card>
 </template>
